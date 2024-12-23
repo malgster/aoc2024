df235f16e7ed081cfc1d7a9df1d2b31089454f2d
malgster <malak.hayar10@gmail.com>
2024-12-23 11:05:48 +0100

    :tada: day one part one

---
+++ src/Day1/list.ts
@@ -0,0 +1,40 @@
+
+import * as fs from 'fs'
+
+
+export function parseColumns() {
+   const data = fs.readFileSync('input.txt', 'utf-8')
+    const lines = data.trim().split('\n')
+
+
+    let columnOne: number[] = []
+    let columnTwo: number[] = []
+
+
+    lines.forEach(line => {
+        const parts = line.split('  ')
+        const num1 = parseInt(parts[0])
+        const num2 = parseInt(parts[1])
+        columnOne.push(num1)
+        columnTwo.push(num2)
+    })
+
+
+    columnOne.sort()
+    columnTwo.sort()
+
+   // console.log("colonne 1",columnOne)
+        //console.log("colonne 2", columnTwo)
+
+    let distance = 0
+
+    for (let i = 0; i < columnOne.length; i++) {
+        distance += Math.abs(columnTwo[i] - columnOne[i])
+    }
+
+    console.log(distance)
+
+
+}
+
+
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+
+import {parseColumns} from "./Day1/list";
+
+parseColumns()
